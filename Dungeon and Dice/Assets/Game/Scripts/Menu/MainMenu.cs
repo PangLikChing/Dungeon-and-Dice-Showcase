68fd3f3b23a29b059fc58214286fc049880133e5
LikChingPang <panglikching@gmail.com>
2022-07-18 16:10:34 -0400

    Basic menu start up sequence done

--- Dungeon and Dice/Assets/Game/Scripts/Menu/MainMenu.cs
+++ Dungeon and Dice/Assets/Game/Scripts/Menu/MainMenu.cs
@@ -4,4 +4,42 @@ using UnityEngine;
 
 public class MainMenu : Menu
 {
+    [Tooltip("Reference to pause menu")]
+    public MenuClassifier pauseMenu;
+    [Tooltip("Reference to hud menu")]
+    public MenuClassifier hudMenu;
+    [Tooltip("Reference to the scene that storges gameplay persistance data")]
+    public SceneReference gameplayGlobalScene;
+    [Tooltip("Reference to world scene in game")]
+    public SceneReference gameWorldScene;
+    private List<string> sceneToBeLoaded = new List<string>();
+
+    /// <summary>
+    /// Callback for start button, load the first gameplay scene, player data scene and hide main menu
+    /// </summary>
+    /// <param name="sceneName">The name of the first gameplay scene</param>
+    public void LoadGameScene()
+    {
+        sceneToBeLoaded.Add(gameplayGlobalScene.ScenePath); //Stay uniform with  startup script
+        sceneToBeLoaded.Add(gameWorldScene.ScenePath);
+        SceneLoader.Instance.OnSceneLoadedEvent += OnGameSceneLoaded;
+        SceneLoader.Instance.LoadScenes(sceneToBeLoaded);
+        this.OnHideMenu();
+    }
+
+    /// <summary>
+    /// Callback for scene loaded event, and show hud menu
+    /// </summary>
+    /// <param name="scenes"></param>
+    public void OnGameSceneLoaded(List<string> scenes)
+    {
+        SceneLoader.Instance.OnSceneLoadedEvent -= OnGameSceneLoaded;
+        sceneToBeLoaded.Clear();
+        MenuManager.Instance.ShowMenu(hudMenu);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
--- Dungeon and Dice/Assets/Game/Scripts/Systems/Core/SceneLoader.cs
+++ Dungeon and Dice/Assets/Game/Scripts/Systems/Core/SceneLoader.cs
@@ -7,14 +7,25 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : Singleton<SceneLoader>
 {
-	public Action<List<string>> OnSceneLoadedEvent;
-
-	public float delayTime = 1.0f;
+    public Action<List<string>> OnSceneLoadedEvent = delegate { };
+    public Action OnSceneUnloadedEvent = delegate { };
+    public string currentActiveScene;
+    public float delayTime = 1.0f;
     private List<string> loadedScenes = new List<string>();
+    private Stack<string> sceneStack = new Stack<string>();
+
+    private void OnEnable()
+    {
+        sceneStack.Push(SceneManager.GetActiveScene().path);
+    }
+
+    private void OnDisable()
+    {
+        sceneStack.Pop();
+    }
 
     // When loading just add a flag for persistence. If true don't add to the loadedScenes
     // Only remove the scenes when you unload
-
     public void LoadScene(string scene, bool showLoadingScreen = true)
     {
         StartCoroutine(loadScene(scene, showLoadingScreen, true));
@@ -42,9 +53,14 @@ public class SceneLoader : Singleton<SceneLoader>
             MenuManager.Instance.HideMenu(MenuManager.Instance.LoadingScreen);
         }
 
-        loadedScenes.Clear();
-        loadedScenes.AddRange(scenes);
-        OnSceneLoadedEvent(loadedScenes);
+        if (OnSceneLoadedEvent != null)
+        {
+            loadedScenes.Clear();
+            loadedScenes.AddRange(scenes);
+            OnSceneLoadedEvent(loadedScenes);
+
+        }
+        SetCurrentSceneActive();
     }
 
     IEnumerator loadScene(string scene, bool showLoadingScreen, bool raiseEvent)
@@ -66,6 +82,7 @@ public class SceneLoader : Singleton<SceneLoader>
             while (sync.isDone == false) { yield return null; }
 
             Application.backgroundLoadingPriority = ThreadPriority.Normal;
+            sceneStack.Push(scene);
 
             yield return new WaitForSeconds(delayTime);
 
@@ -75,12 +92,14 @@ public class SceneLoader : Singleton<SceneLoader>
             }
         }
 
-        if (raiseEvent)
+        if (raiseEvent && OnSceneLoadedEvent != null)
         {
             loadedScenes.Clear();
             loadedScenes.Add(scene);
             OnSceneLoadedEvent(loadedScenes);
+
         }
+        SetCurrentSceneActive();
     }
 
     // 4 Methods:
@@ -89,26 +108,46 @@ public class SceneLoader : Singleton<SceneLoader>
     //		- Support to unload multiple (Coroutine)
     //	- Actual Unload of scenes.
 
-    public void UnloadScene(string scene)
+    public void UnloadScene(string scene, bool showLoadingScreen = true)
     {
-        StartCoroutine(unloadScene(scene));
+        StartCoroutine(unloadScene(scene, showLoadingScreen, true));
     }
 
-    public void UnloadScenes(List<string> scenes)
+    public void UnloadScenes(List<string> scenes, bool showLoadingScreen = true)
     {
-        StartCoroutine(unloadScenes(scenes));
+        StartCoroutine(unloadScenes(scenes, showLoadingScreen));
     }
 
-    IEnumerator unloadScenes(List<string> scenes)
+    IEnumerator unloadScenes(List<string> scenes, bool showLoadingScreen)
     {
+        if (showLoadingScreen)
+        {
+            MenuManager.Instance.ShowMenu(MenuManager.Instance.LoadingScreen);
+        }
         foreach (string scene in scenes)
         {
-            yield return StartCoroutine(unloadScene(scene));
+            yield return StartCoroutine(unloadScene(scene, false, false));
         }
+        if (showLoadingScreen)
+        {
+            MenuManager.Instance.HideMenu(MenuManager.Instance.LoadingScreen);
+        }
+        if (OnSceneUnloadedEvent != null)
+        {
+            OnSceneUnloadedEvent();
+        }
+        SetCurrentSceneActive();
     }
 
-    IEnumerator unloadScene(string scene)
+    IEnumerator unloadScene(string scene, bool showLoadingScreen, bool raiseEvent)
     {
+        if (showLoadingScreen)
+        {
+            MenuManager.Instance.ShowMenu(MenuManager.Instance.LoadingScreen);
+        }
+
+        yield return new WaitForSeconds(delayTime);
+
         AsyncOperation sync = null;
 
         try
@@ -127,5 +166,29 @@ public class SceneLoader : Singleton<SceneLoader>
 
         sync = Resources.UnloadUnusedAssets();
         while (sync.isDone == false) { yield return null; }
+
+        sceneStack.Pop();
+
+        yield return new WaitForSeconds(delayTime);
+
+        if (showLoadingScreen)
+        {
+            MenuManager.Instance.HideMenu(MenuManager.Instance.LoadingScreen);
+        }
+
+        if (raiseEvent && OnSceneUnloadedEvent != null)
+        {
+            OnSceneUnloadedEvent();
+
+        }
+        SetCurrentSceneActive();
+    }
+
+    private void SetCurrentSceneActive()
+    {
+        Scene topScene = SceneManager.GetSceneByPath(sceneStack.Peek());
+        if (topScene.IsValid())
+            SceneManager.SetActiveScene(topScene);
+        currentActiveScene = sceneStack.Peek();
     }
 }
