2f7e9154474d524e0dda882ddf05d5df3cda1199
LikChingPang <panglikching@gmail.com>
2022-07-28 23:22:24 -0400

    Created Skill Buttons and made skill dropbox rest exclusive

--- Dungeon and Dice/Assets/Game/Scripts/Drag and Drop/SkillDropboxDropHandler.cs
+++ Dungeon and Dice/Assets/Game/Scripts/Drag and Drop/SkillDropboxDropHandler.cs
@@ -14,6 +14,22 @@ public class SkillDropboxDropHandler : DropHandler
     [Tooltip("Raise when there is no current target")]
     public UnityEvent<Transform> TryChangeTarget;
 
+    Skill skill;
+
+    void Start()
+    {
+        try
+        {
+            // Initialize
+            skill = transform.parent.GetComponent<Skill>();
+        }
+        catch
+        {
+            // Throw a debug message
+            Debug.Log("My parent is not a skill");
+        }
+    }
+
     public override void OnDrop(PointerEventData eventData)
     {
         // Execute the base DropHandler class's code
@@ -24,135 +40,28 @@ public class SkillDropboxDropHandler : DropHandler
         {
             try
             {
-                // Get a reference to the parent's skill component
-                Skill skill = transform.parent.GetComponent<Skill>();
-
-                try
+                // If the current encounter is a rest event
+                if (GameManager.Instance.currentEncounter.GetType() == typeof(Resting))
                 {
-                    // If the current encounter is a battle
-                    if (GameManager.Instance.currentEncounter.GetType() == typeof(Battle))
+                    // If the die size of the dropped object and the skill matches
+                    if (eventData.pointerDrag.GetComponent<Dice>().dieSize == skill.dieSize)
                     {
-                        // If the skill is a damaging skill
-                        if (skill.skillData.GetType() == typeof(DamageSkillData))
-                        {
-                            // If there is no enemy target yet
-                            if (GameManager.Instance.currentEnemyTarget == null)
-                            {
-                                try
-                                {
-                                    // Try to set the target at the first enemy in the enemy list in the game manager
-                                    TryChangeTarget.Invoke(GameManager.Instance.enemyList[0].transform);
-                                }
-                                catch
-                                {
-                                    // Throw a debug message
-                                    Debug.Log("There is no enemy left!");
-                                }
-                            }
-                        }
-                        // If the skill is a healing or shielding skill
-                        else if (skill.skillData.GetType() == typeof(HealingSkillData) || skill.skillData.GetType() == typeof(ShieldingSkillData))
-                        {
-                            // If there is no player target yet
-                            if (GameManager.Instance.currentPlayerTarget == null)
-                            {
-                                try
-                                {
-                                    // Try to set the target at the first player in the player list in the game manager
-                                    TryChangeTarget.Invoke(GameManager.Instance.playerList[0].transform);
-                                }
-                                catch
-                                {
-                                    // Throw a debug message
-                                    Debug.Log("There is no player left!");
-                                }
-                            }
-                        }
-
-                        // Roll the dice and execute the skill
-                        StartCoroutine(RollDiceAndExecuteSkill(skill));
-                    }
-                    // If the current encounter is a rest event
-                    else if (GameManager.Instance.currentEncounter.GetType() == typeof(Resting))
-                    {
-                        // If the die size of the dropped object and the skill matches
-                        if (eventData.pointerDrag.GetComponent<Dice>().dieSize == skill.dieSize)
-                        {
-                            // Upgrade the skill
-                            skill.UpgradeSkill();
-                        }
-                    }
-                    // Else
-                    else
-                    {
-                        // Throw a debug message
-                        Debug.Log("Unknown encounter type!");
+                        // Upgrade the skill
+                        skill.UpgradeSkill();
                     }
                 }
-                catch
+                // Else
+                else
                 {
                     // Throw a debug message
-                    Debug.Log("There is no encounter!");
+                    Debug.Log("Unknown encounter type!");
                 }
             }
             catch
             {
                 // Throw a debug message
-                Debug.Log("My parent is not a skill");
+                Debug.Log("There is no encounter!");
             }
-        }
-    }
-
-    IEnumerator RollDiceAndExecuteSkill(Skill skill)
-    {
-        // Intialize a temp transform prefeb to store the die object prefeb
-        Dice die = null;
-
-        // Determine the die object prefeb needed
-        for (int i = 0; i < DiceManager.Instance.dieMatching.Length; i++)
-        {
-            // If the die size in the skill is the same as the one in dice manager
-            if (skill.dieSize == DiceManager.Instance.dieMatching[i].dieSize)
-            {
-                die = DiceManager.Instance.dieMatching[i].diePrefeb;
-            }
-        }
-
-        // Instantiate a die object for every die needed and roll them
-        for (int i = 0; i < skill.numberOfDie; i++)
-        {
-            // Instantiate the die
-            Dice dice = Instantiate(die, DiceRollTray.Instance.transform);
-
-            // Roll the die
-            dice.RollDie();
-        }
-
-        // Wait for the die to finish the roll
-        yield return new WaitForSeconds(die.rollingTime);
-
-        // Initialzie a temp int to store the roll result
-        int rollResults = 0;
-
-        // Calculate the total amount of roll results
-        for (int i = 0; i < DiceRollTray.Instance.transform.childCount; i++)
-        {
-            rollResults += DiceRollTray.Instance.transform.GetChild(i).GetComponent<Dice>().rollResult;
-        }
-
-        // Execute the skill
-        // TODO : Calculate damage here
-        // If the skill is a damaging skill
-        if (skill.skillData.GetType() == typeof(DamageSkillData))
-        {
-            // Execute the skill on the targeted enemy, no matter it is an AOE or not
-            skill.ExecuteSkill(GameManager.Instance.currentPlayer, GameManager.Instance.currentEnemyTarget, rollResults);
-        }
-        // If the skill is a healing or shielding skill
-        else if (skill.skillData.GetType() == typeof(HealingSkillData) || skill.skillData.GetType() == typeof(ShieldingSkillData))
-        {
-            // Execute the skill on the targeted player, no matter it is an AOE or not
-            skill.ExecuteSkill(GameManager.Instance.currentPlayer, GameManager.Instance.currentPlayerTarget, rollResults);
         }
     }
 }
---
+++ Dungeon and Dice/Assets/Testing/Scripts/Skill/SkillButton.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Skill))]
+public class SkillButton : MonoBehaviour
+{
+    [Tooltip("Raise when there is no current target")]
+    public UnityEvent<Transform> TryChangeTarget;
+
+    public void ExecuteSkill()
+    {
+        // Get a reference to the parent's skill component
+        Skill skill = transform.GetComponent<Skill>();
+
+        try
+        {
+            // If the current encounter is a battle
+            if (GameManager.Instance.currentEncounter.GetType() == typeof(Battle))
+            {
+                // If the skill is a damaging skill
+                if (skill.skillData.GetType() == typeof(DamageSkillData))
+                {
+                    // If there is no enemy target yet
+                    if (GameManager.Instance.currentEnemyTarget == null)
+                    {
+                        try
+                        {
+                            // Try to set the target at the first enemy in the enemy list in the game manager
+                            TryChangeTarget.Invoke(GameManager.Instance.enemyList[0].transform);
+                        }
+                        catch
+                        {
+                            // Throw a debug message
+                            Debug.Log("There is no enemy left!");
+                        }
+                    }
+                }
+                // If the skill is a healing or shielding skill
+                else if (skill.skillData.GetType() == typeof(HealingSkillData) || skill.skillData.GetType() == typeof(ShieldingSkillData))
+                {
+                    // If there is no player target yet
+                    if (GameManager.Instance.currentPlayerTarget == null)
+                    {
+                        try
+                        {
+                            // Try to set the target at the first player in the player list in the game manager
+                            TryChangeTarget.Invoke(GameManager.Instance.playerList[0].transform);
+                        }
+                        catch
+                        {
+                            // Throw a debug message
+                            Debug.Log("There is no player left!");
+                        }
+                    }
+                }
+
+                // Roll the dice and execute the skill
+                StartCoroutine(RollDiceAndExecuteSkill(skill));
+            }
+        }
+        catch
+        {
+            // Throw a debug message
+            Debug.Log("Unknown encounter type!");
+        }
+    }
+    IEnumerator RollDiceAndExecuteSkill(Skill skill)
+    {
+        // Intialize a temp transform prefeb to store the die object prefeb
+        Dice die = null;
+
+        // Determine the die object prefeb needed
+        for (int i = 0; i < DiceManager.Instance.dieMatching.Length; i++)
+        {
+            // If the die size in the skill is the same as the one in dice manager
+            if (skill.dieSize == DiceManager.Instance.dieMatching[i].dieSize)
+            {
+                die = DiceManager.Instance.dieMatching[i].diePrefeb;
+            }
+        }
+
+        // Instantiate a die object for every die needed and roll them
+        for (int i = 0; i < skill.numberOfDie; i++)
+        {
+            // Instantiate the die
+            Dice dice = Instantiate(die, DiceRollTray.Instance.transform);
+
+            // Roll the die
+            dice.RollDie();
+        }
+
+        // Wait for the die to finish the roll
+        yield return new WaitForSeconds(die.rollingTime);
+
+        // Initialzie a temp int to store the roll result
+        int rollResults = 0;
+
+        // Calculate the total amount of roll results
+        for (int i = 0; i < DiceRollTray.Instance.transform.childCount; i++)
+        {
+            rollResults += DiceRollTray.Instance.transform.GetChild(i).GetComponent<Dice>().rollResult;
+        }
+
+        // Execute the skill
+        // TODO : Calculate damage here
+        // If the skill is a damaging skill
+        if (skill.skillData.GetType() == typeof(DamageSkillData))
+        {
+            // Execute the skill on the targeted enemy, no matter it is an AOE or not
+            skill.ExecuteSkill(GameManager.Instance.currentPlayer, GameManager.Instance.currentEnemyTarget, rollResults);
+        }
+        // If the skill is a healing or shielding skill
+        else if (skill.skillData.GetType() == typeof(HealingSkillData) || skill.skillData.GetType() == typeof(ShieldingSkillData))
+        {
+            // Execute the skill on the targeted player, no matter it is an AOE or not
+            skill.ExecuteSkill(GameManager.Instance.currentPlayer, GameManager.Instance.currentPlayerTarget, rollResults);
+        }
+    }
+}
